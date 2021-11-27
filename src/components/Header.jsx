92befa6c72373a17b7eb76787d5684c918f6a02b
TG2408 <tanmaygupta2408@gmail.com>
2021-11-27 20:30:59 +0530

    Header component finish

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { AppBar, Toolbar } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const useStyles = makeStyles({
+    header: {
+        background: "#fff", 
+        height: "70px"
+    },
+    logo: {
+        height: "55px",
+        margin: "auto",
+        paddingRight: 70
+    },
+    menu: {
+        color: "#000"
+    }
+})
+
+const Header = () => {
+
+    const classes = useStyles();
+    const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
+
+    return(
+        <AppBar className={classes.header}>
+            <Toolbar>
+                <MenuIcon className={classes.menu}/>
+                <img src={url} alt="logo" className={classes.logo} />
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
+
